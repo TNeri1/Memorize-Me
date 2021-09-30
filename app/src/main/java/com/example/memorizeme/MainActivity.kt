aad1c415ffc09cbbd2a5449792625e1c0a8e8390
TNeri1 <triborneri@pm.me>
2021-09-29 22:13:40 -0600

    App with Cards in Horizontal View

--- app/src/main/java/com/example/memorizeme/MainActivity.kt
+++ app/src/main/java/com/example/memorizeme/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.memorizeme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.memorizeme.Adapter.Horizontal_RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: Horizontal_RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter = Horizontal_RecyclerView()
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
+
     }
 }
